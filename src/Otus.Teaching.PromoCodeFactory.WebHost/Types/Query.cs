26a23efce4daa17447f61fe2da791f89a96da58f
Дмитрий Кулик <liamkukid@gmail.com>
2020-11-09 22:12:50 +0300

    запрос на получение по id

--- src/Otus.Teaching.PromoCodeFactory.WebHost/Types/Query.cs
+++ src/Otus.Teaching.PromoCodeFactory.WebHost/Types/Query.cs
@@ -2,6 +2,8 @@
 using HotChocolate.Types;
 using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
 using Otus.Teaching.PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using Otus.Teaching.PromoCodeFactory.DataAccess;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,6 +16,13 @@ namespace Otus.Teaching.PromoCodeFactory.WebHost.Types
         public async Task<IQueryable<Customer>> GetCustomersAsync([Service] IRepository<Customer> customerRepository)
         {
             return (await customerRepository.GetAllAsync()).AsQueryable();
+        }
+
+        [UseFirstOrDefault]
+        [UseSelection]
+        public IQueryable<Customer> GetCustomerById([Service] DataContext context, Guid id)
+        {
+            return context.Customers.Where(x => x.Id == id);
         }
     }
 }
